e5fe406d87816f8a995caf3d74050e1510af3223
Kelyng Bonifacio <54644097+theaguacate9341@users.noreply.github.com>
2019-09-04 12:23:19 -0400

    Add files via upload

---
+++ NameGetter.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ * @author Student
+ * description:
+ * created:
+ *
+ */
+//import Scanner class
+import java.util.Scanner;
+public class NameGetter {
+//main method,
+	//the program starts at the main method
+	public static void main(String[] args) {
+	//Create Scanner Object
+	Scanner input = new Scanner(System.in);
+	//Prompts the user for an input
+	System.out.println("Enter Your first and last name");
+	//Waits for the input from the user
+	//and grabs it, then store the variable
+	String fname = input.next();
+	String lname = input.next();
+	//Reply to user
+	System.out.println("Thank you, "+fname);
+	System.out.println("Oh your last name is "+lname+"!!");
+} }
