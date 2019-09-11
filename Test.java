277000623af286771dd93c3c464ed450f481f36e
Kelyng Bonifacio <54644097+theaguacate9341@users.noreply.github.com>
2019-09-11 12:30:59 -0400

    Week 3
    Testing output and flags

---
+++ Test.java
@@ -0,0 +1,27 @@
+/** 
+ * @author Kelyng Bonifacio
+ */
+public class Test {
+	public static void main(String[] args) {
+//		System.out.print(78+98); // 176
+//		System.out.print('a'+'b'); // 195
+//		System.out.println('a'-'b'); // -1
+//		System.out.println('0'+'0'); // 96
+//		System.out.println('a'+1); // 98
+//		System.out.println('a'); // a
+		
+		int num = 3;
+		double dollars = 15.60;
+		String name1 = "Kanye West";
+		String name2 = "Jay Z";
+		
+		System.out.printf("I have %d cars %n", num);// I have 10 cars
+		System.out.printf(" Maria has %n $%.2f in her pocket", dollars); 
+		
+		System.out.printf("%n %s and %s was the best collab for %d", name1,name2, num);
+
+
+	}
+
+
+}
