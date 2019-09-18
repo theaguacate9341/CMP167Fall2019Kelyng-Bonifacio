3bdb275c0452542fdf032173f4aae8b14b583ac4
Kelyng Bonifacio <54644097+theaguacate9341@users.noreply.github.com>
2019-09-18 12:32:46 -0400

    Update Unit4p2.java

--- Unit4p2.java
+++ Unit4p2.java
@@ -15,14 +15,15 @@ public class Unit4p2 {
 			System.out.print("Enter b: ");
 			b = in.nextInt();
 			
-			if (a%b==0) {
-				System.out.printf("%d is divisible by %d ", a, b);
-			}
-			else {
-				System.out.printf("%d is not divisible by %d ", a,b);
-			}
-			
-			
+		isDivisible(a,b);
+	}
+	public static void isDivisible(int num1, int num2) {
+		if (num1%num2==0) {
+			System.out.printf("%d is divisible by %d ", num1, num2);
+		}
+		else {
+			System.out.printf("%d is not divisible by %d ", num1,num2);
+		}
 	}
 
 }
