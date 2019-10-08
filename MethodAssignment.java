030c818d91c2e734b5cf5fc6d3d5111d9a51b5d1
Kelyng Bonifacio <54644097+theaguacate9341@users.noreply.github.com>
2019-10-08 10:46:31 -0400

    week6

---
+++ MethodAssignment.java
@@ -0,0 +1,13 @@
+/**
+ * @author Kelyng Bonifacio
+ */
+public class MethodAssignment {
+	public static void main (String [] args) {
+		}
+	public static int product3(int a, int b, int c) {
+		return a*b*c;
+		
+	}
+	public static 
+
+}
